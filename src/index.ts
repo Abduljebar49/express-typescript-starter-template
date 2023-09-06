2161ca8c77b49b9b7fc4735b7be201b52c4e78a2
Abduljebar Sani <Abduljebar49@gmail.com>
2023-09-06 16:59:38 +0300

    route added to express server

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import logger from 'morgan';
-
+import userRoute from './routes/user.js'
 dotenv.config();
 
 const port = process.env.PORT;
@@ -10,5 +10,5 @@ const app: Express = express();
 app.use(express.json());
 app.use(logger('dev'));
 app.use(express.static("./public"));
+app.use('/api',userRoute);
 app.listen(3000);
-console.log("express server has successfully initialized");
